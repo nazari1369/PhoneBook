4f9f42e09b9506bea99d8c6b5c3875a28d44f1c8
nazari1369 <nazari1369@gmail.com>
2023-06-07 16:53:11 +0330

    1

--- PhoneBook_Project/PhoneBook/templatetags/crispy-form.py
+++ PhoneBook_Project/PhoneBook/templatetags/crispy-form.py
@@ -35,3 +35,35 @@ def link(href="", extra_classes=""):
 @register.simple_tag
 def endlink():
     return format_html("</a>")
+
+@register.simple_tag
+def table(default_id, extra_classes=""):
+    return format_html('<table id="{}" class="{}">', default_id, extra_classes)
+
+@register.simple_tag
+def endtable():
+    return format_html("</table>")
+
+@register.simple_tag
+def tbody(extra_classes=""):
+    return format_html('<tbody class="{}">', extra_classes)
+
+@register.simple_tag
+def endtbody():
+    return format_html("</tbody>")
+
+@register.simple_tag
+def table_row(extra_classes=""):
+    return format_html('<tr class="{}">', extra_classes)
+
+@register.simple_tag
+def endtable_row():
+    return format_html("</tr>")
+
+@register.simple_tag
+def table_col(extra_classes=""):
+    return format_html('<td class="{}">', extra_classes)
+
+@register.simple_tag
+def endtable_col():
+    return format_html("</td>")
