04229b11ce4d75d5474d80c7fb6436df50f82f90
nazari1369 <nazari1369@gmail.com>
2023-06-08 14:49:13 +0330

    1

--- PhoneBook_Project/PhoneBook/templatetags/crispy-form.py
+++ PhoneBook_Project/PhoneBook/templatetags/crispy-form.py
@@ -3,22 +3,22 @@ from django.utils.html import format_html
 
 register = template.Library()
 
-@register.simple_tag
-def row(extra_classes=""):
-    return format_html('<div class="row {}">', extra_classes)
-
-
-@register.simple_tag
-def endrow():
-    return format_html("</div>")
-
-@register.simple_tag
-def col(extra_classes=""):
-    return format_html('<div class="col {}">', extra_classes)
-
-@register.simple_tag
-def endcol():
-    return format_html("</div>")
+# @register.simple_tag
+# def row(extra_classes=""):
+#     return format_html('<div class="row {}">', extra_classes)
+#
+#
+# @register.simple_tag
+# def endrow():
+#     return format_html("</div>")
+#
+# @register.simple_tag
+# def col(extra_classes=""):
+#     return format_html('<div class="col {}">', extra_classes)
+#
+# @register.simple_tag
+# def endcol():
+#     return format_html("</div>")
 
 @register.simple_tag
 def para(extra_classes=""):
@@ -38,7 +38,7 @@ def endlink():
 
 @register.simple_tag
 def table(default_id, extra_classes=""):
-    return format_html('<table id="{}" class="{}">', default_id, extra_classes)
+    return format_html('<table id="{}" class="{}" style="90%;">', default_id, extra_classes)
 
 @register.simple_tag
 def endtable():
